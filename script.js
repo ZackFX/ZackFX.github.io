388c770fdf07a572615751dde62c6a7ad20683e7
MacBook Pro <macbookpro@MacBook-Pro-3.local>
2019-03-24 14:50:04 +0100

    task5 not finished

--- script.js
+++ script.js
@@ -19,4 +19,11 @@ function init() {
     image.src="flower2.png";
     document.getElementById("new_element").appendChild(image);
 
+    let spans = document.getElementById("rainbow").toARRAY();
+    let colors = ["red", "orange","yellow","green","blue","purple","pink"];
+
+    for (var i = spans.length - 1; i >= 0; i--) {
+        spans[i].style.color = colors[i];
+    }
+
 }
