5e7f465b74e6bfa75bedb38576feb07d1dcfb562
MacBook Pro <macbookpro@MacBook-Pro-3.local>
2019-03-24 14:23:11 +0100

    task4

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ function greet() {
 }
 
 function init() {
+    
     document.getElementById("first").src = "flower2.png"; 
+    
     // document.getElementById("container").getElementsByTagName("img")[1].src = "flower2.png";
     // document.getElementById("container").getElementsByTagName("img")[3].src = "flower2.png";
+    
     let images = document.getElementById("container").getElementsByTagName("img");
     images[1].src="flower2.png";
     images[3].src="flower2.png";
+    
     document.getElementById("task3").innerHTML = "<div><p>Your are great!</p></div>";
-
+    
+    let image = document.createElement("img");
+    image.src="flower2.png";
+    document.getElementById("new_element").appendChild(image);
 
 }
