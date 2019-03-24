4bc4f0fd38a523734274ef1d33036118957ec092
MacBook Pro <macbookpro@MacBook-Pro-3.local>
2019-03-24 13:29:48 +0100

    DOM task 1 and 2

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function greet() {
     window.alert("Hello everyone");
 }
+
+function init() {
+    document.getElementById("first").src = "flower2.png"; 
+    document.getElementById("container").getElementsByTagName("img")[1].src = "flower2.png";
+}
